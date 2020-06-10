596d2883dd3d87ba38e9cf03b4e15941513a4546
herb.nb-herb <herbert.kreutzer@gmail.com>
2020-06-10 16:08:02 +0200

    Generic Functions, lesson 5.15

--- src/aquarium/generics/Aquarium.kt
+++ src/aquarium/generics/Aquarium.kt
@@ -49,13 +49,28 @@ fun addItemTo(aquarium: Aquarium<WaterSupply>) {
     println("item added to ${aquarium.waterSupply::class.simpleName} ${aquarium::class.simpleName}")
 }
 
+fun <T : WaterSupply> isWaterClean(aquarium: Aquarium<T>) {
+    println("${aquarium.waterSupply::class.simpleName} aquarium needs processing: ${aquarium.waterSupply.needProcessing}")
+}
+
+inline fun <reified R : WaterSupply> Aquarium<*>.hasWaterSupplyOfType() = this is R
+inline fun <reified T : WaterSupply> WaterSupply.isOfType() = this is T
+fun WaterSupply.type() = this::class.simpleName
+
 fun genericExample() {
     val aquarium = Aquarium(TapWater())
+    println("${aquarium.hasWaterSupplyOfType<TapWater>()}")
+    println("${aquarium.waterSupply.isOfType<LakeWater>()}")
+    println("${aquarium.waterSupply.type()}")
+    isWaterClean(aquarium)
     aquarium.addWater(TapWaterCleaner())
+    isWaterClean(aquarium)
     addItemTo(aquarium)
 
     val aquarium2 = Aquarium(LakeWater())
+    isWaterClean(aquarium2)
     aquarium2.addWater(LakeWaterCleaner())
+    isWaterClean(aquarium2)
     addItemTo(aquarium2)
 }
 
