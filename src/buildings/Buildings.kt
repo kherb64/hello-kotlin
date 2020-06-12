01ab94c59721f42fbb42150b5ca2e4efc04cadcb
herb.nb-herb <herbert.kreutzer@gmail.com>
2020-06-12 10:32:50 +0200

    Generic Functions, lesson 5.16

--- src/buildings/Buildings.kt
+++ src/buildings/Buildings.kt
@@ -1,18 +1,19 @@
 package co.at.sdt.herb.kotlin.buildings
 
-open class BaseBuildingMaterial() {
+open class BaseBuildingMaterial {
     open val numberNeeded = 1
 }
 
-class Wood(): BaseBuildingMaterial() {
+class Wood : BaseBuildingMaterial() {
     override val numberNeeded = 4
 }
-class Brick(): BaseBuildingMaterial() {
+
+class Brick : BaseBuildingMaterial() {
     override val numberNeeded = 8
 }
 
-class Building<out T: BaseBuildingMaterial>(val material: T) {
-    val baseMaterialsNeeded = 100
+class Building<out T : BaseBuildingMaterial>(val material: T) {
+    private val baseMaterialsNeeded = 100
     val actualMaterialsNeeded = baseMaterialsNeeded * material.numberNeeded
 
     fun build() {
@@ -20,7 +21,17 @@ class Building<out T: BaseBuildingMaterial>(val material: T) {
     }
 }
 
+fun <T : BaseBuildingMaterial> Building<T>.isSmall(): Boolean = actualMaterialsNeeded < 500
+
+fun <T : BaseBuildingMaterial> Building<T>.description(): String {
+    return "${if (isSmall()) "small" else "large"} ${material::class.simpleName} building"
+}
+
 fun main() {
-    Building(Wood()).build()
-    Building(Brick()).build()
+    val wood = Building(Wood())
+    wood.build()
+    println(wood.description())
+    val brick = Building(Brick())
+    brick.build()
+    println(brick.description())
 }
