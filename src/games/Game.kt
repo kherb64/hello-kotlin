8d1e85a6b29616062aaa043386276a12444d1414
herb.nb-herb <herbert.kreutzer@gmail.com>
2020-06-15 13:28:40 +0200

    Game, lesson 6.8

--- src/games/Game.kt
+++ src/games/Game.kt
@@ -5,10 +5,10 @@ enum class Directions { NORTH, SOUTH, EAST, WEST, START, END }
 class Game {
     var path = mutableListOf<Directions>(Directions.START)
 
-    val north = { path.add(Directions.NORTH); this }
-    val south = { path.add(Directions.SOUTH); this }
-    val east = { path.add(Directions.EAST); this }
-    val west = { path.add(Directions.WEST); this }
+    val north = { path.add(Directions.NORTH) }
+    val south = { path.add(Directions.SOUTH) }
+    val east = { path.add(Directions.EAST) }
+    val west = { path.add(Directions.WEST) }
 
     val end = {
         path.add(Directions.END)
@@ -18,11 +18,32 @@ class Game {
         false
     }
 
+    fun makeMove(command: String?) {
+        when (command) {
+            "n" -> move(north)
+            "s" -> move(south)
+            "e" -> move(east)
+            "w" -> move(west)
+            else -> move(end)
+        }
+    }
+}
+
+fun move(where: () -> Boolean) {
+    where.invoke()
 }
 
 fun main() {
     val game = Game()
     println(game.path)
-    game.north().east().south().west().end()
+    /* game.north()
+    game.east()
+    game.south()
+    game.west()
+    game.end() */
+    while (!game.path.contains(Directions.END)) {
+        game.makeMove(readLine())
+        println(game.path)
+    }
     println(game.path)
 }
